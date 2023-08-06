e1d59c527de5e85451dc7e67510c4d4d01cf8de9
tamtam-fitness <yu.tamura@raksul.com>
2023-08-07 00:54:57 +0900

    del unused files

--- src/module/temp_class.py
+++
@@ -1,8 +0,0 @@
-# please delete module dir because of meaningless code
-from src.common import logger
-
-
-class TempClass:
-    def add_temp(self, input_str: str) -> str:
-        logger.info("temp")
-        return input_str + "temp"
